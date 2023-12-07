510d136773ab831dfe689bb8b2b24469931bd4bd
Xx-Diego-XX-Agustin-xx-Garcia-xX <720791@student.dusd.net>
2023-12-07 13:49:55 -0800

    Updated A Star Pathfinder
    Updated AStarPathfinder.cs to give it rudimentary AI.

--- AStarAlgorithmGarcia/Assets/Scripts/AStarPathfinder.cs
+++ AStarAlgorithmGarcia/Assets/Scripts/AStarPathfinder.cs
@@ -21,7 +21,7 @@ public class PathMarker
     }
     public override bool Equals(object obj)
     {
-        if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+        if((obj == null) || !this.GetType().Equals(obj.GetType()))
         { 
             return false;
         }
@@ -64,9 +64,9 @@ public class AStarPathfinder : MonoBehaviour
         List<MapLocation> locations = new List<MapLocation>();
         for(int z = 1; z < maze.depth; z++)
         {
-            for (int x = 1; x < maze.depth; x++)
+            for(int x = 1; x < maze.depth; x++)
             {
-                if (maze.map[x,z] != 1)
+                if(maze.map[x,z] != 1)
                 {
                     locations.Add(new MapLocation(x,z));
                 }
@@ -84,21 +84,101 @@ public class AStarPathfinder : MonoBehaviour
     }
     void Search(PathMarker thisNode)
     {
-        if (thisNode.Equals(endNode))
+        if(thisNode == null)
+        {
+            return;
+        }
+        if(thisNode.Equals(endNode))
         {
             done = true;
             return;
         }
+        foreach(MapLocation dir in maze.directions)
+        {
+            MapLocation neighbor = dir + thisNode.location;
+            if(maze.map[neighbor.x, neighbor.z] == 1)
+            {
+                continue;
+            }
+            if(neighbor.x < 1 || neighbor.x >= maze.width || neighbor.z < 1 || neighbor.z >= maze.depth)
+            {
+                continue;
+            }
+            if(IsClosed(neighbor))
+            {
+                continue;
+            }
+            float A = Vector2.Distance(thisNode.location.ToVector(), neighbor.ToVector()) + thisNode.A;
+            float B = Vector2.Distance(neighbor.ToVector(), endNode.location.ToVector());
+            float C = A + B;
+            GameObject pathBlock = Instantiate(path, new Vector3(neighbor.x * maze.scale, 0, neighbor.z * maze.scale), Quaternion.identity);
+            TextMesh[] values = pathBlock.GetComponentsInChildren<TextMesh>();
+            values[0].text = "G: " + A.ToString("0.00");
+            values[1].text = "H: " + B.ToString("0.00");
+            values[2].text = "F: " + C.ToString("0.00");
+            if(!UpdateMarker(neighbor, A, B, C, thisNode))
+            {
+                open.Add(new PathMarker(neighbor, A, B, C, pathBlock, thisNode));
+            }
+        }
+        open = open.OrderBy(pathMarker => pathMarker.C).ThenBy(x => x.B).ToList<PathMarker>();
+        PathMarker smallMarker = (PathMarker) open.ElementAt(0);
+        closed.Add(smallMarker);
+        open.RemoveAt(0);
+        smallMarker.markerChild.GetComponent<Renderer>().material = closedMaterial;
+        lastPosition = smallMarker;
+    }
+    bool UpdateMarker(MapLocation position, float a, float b, float c, PathMarker markpath)
+    {
+        foreach(PathMarker pathMarker in open)
+        {
+            pathMarker.A = a;
+            pathMarker.B = b;
+            pathMarker.C = c;
+            pathMarker.markerParent = markpath;
+            return true;
+        }
+        return false;
+    }
+    bool IsClosed(MapLocation marker)
+    {
+        foreach(PathMarker pathMarker in closed)
+        {
+            if(pathMarker.location.Equals(marker))
+            {
+                return true;
+            }
+        }
+        return false;
     }
     void Start()
     {
-        
+        Debug.Log("Press W before you press S");
+    }
+    void GetPath()
+    {
+        MarkerRemoval();
+        PathMarker begin = lastPosition;
+        while(!startNode.Equals(begin) && begin != null)
+        {
+            Instantiate(path, new Vector3(begin.location.x * maze.scale, 0, begin.location.z * maze.scale), Quaternion.identity);
+            begin = begin.markerParent;
+        }
+        Instantiate(path, new Vector3(startNode.location.x * maze.scale, 0, startNode.location.z * maze.scale), Quaternion.identity);
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.W))
         {
             BeginSearch();
+        }
+        if(Input.GetKeyDown(KeyCode.S) && !done)
+        {
+            Search(lastPosition);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GetPath();
         }
     }
 }
